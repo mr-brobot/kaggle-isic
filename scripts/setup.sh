d428a468b6ef2fc5bc09b966869db67986431a8f
Josh Wiley <josh@cloudbend.dev>
2025-01-11 10:42:16 -0800

    feat(devcontainer): init

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install --upgrade pip
+
+# https://jupyter.org/install
+pip install notebook
+pip install ipywidgets
+pip install matplotlib
+
+
+# https://pytorch.org/
+pip install torch torchvision torchaudio
+
+# wandb
+pip install wandb
